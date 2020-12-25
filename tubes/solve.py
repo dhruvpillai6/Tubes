9cdd6ad79245ab964efc2b073e2a4055e5ad4c2a
Lucas Lofaro <lucaslofaro@gmail.com>
2020-12-25 03:12:36 -0800

    refactor: clean up data structures

--- tubes/solve.py
+++ tubes/solve.py
@@ -31,46 +31,29 @@ def solve(game_input):
     :param game_input: Game object
     :return: #TODO: return solution
     """
-    # Instantiate the starting node, which corresponds to the starting state of the
-    # game. Nodes will be implemented as dictionaries.
-    # game stores the game object
-    # solved is a boolean indicating whether or not the node is solved
-    # The score stores the color_score. This is not yet used, but could be leveraged
-    # for less naive move selection.
-    # Moves is a list of moves used to arrive at the current state from the starting
-    # state
-    # hash is a hash of the current game state, used to compare the current state to
-    # previously seen states and avoid unnecessary computation
-    # legal moves is a dictionary of moves, which result in future nodes for the BFS
-    # algorithm
-    tree = {
+    # Queue and visited are utilized as standard BFS structures. The queue stores the
+    # unevaluated states, and the visited stores hashes of previously seen states.
+    queue = [{
         'game': game_input,
         'solved': game_input._solved,
         'score': game_input._color_score,
         'moves': [],
         'hash': hash(game_input),
         'legal_moves': {}
-    }
-    # Queue and visited are utilized as standard BFS structures. The queue stores the
-    # unevaluated states, and the visited stores hashes of previously seen states.
-    queue = []
-    visited = []
-    orig_dict = tree
+    }]
+    visited = set()
     num_moves_tried = 0
 
-    # First, append the starting state to the queue.
-    queue.append(orig_dict)
     while queue:
         num_moves_tried += 1
 
         # take the first element in the queue and get read to analyze. Also,
         # note that we have visited this node
         node = queue.pop(0)
-        visited.append(hash(node['game']))
+        visited.add(node['game'])
 
         # In order to prevent inappropriate copying of information from one node to
         # another, copy the node information to avoid making changes to the original.
-        level_dict = deepcopy(node)
         if node['solved']:
             print('solved!')
             print(active_dict['moves'])
@@ -84,22 +67,23 @@ def solve(game_input):
 
         # Evaluate each legal move for its effect on the state of the game
         for move in legal_moves:
-            active_dict = deepcopy(level_dict)
+            active_dict = deepcopy(node)
             if not active_dict['legal_moves']:
                 active_dict['legal_moves'] = {move: {}}
             else:
                 active_dict['legal_moves'][move] = {}
             cur_moves = active_dict['moves']
             active_dict = active_dict['legal_moves'][move]
-            active_dict['game'] = deepcopy(game)
-            active_game = active_dict['game']
-            active_game._push_move(move[0], move[1])
-            active_dict['solved'] = active_game._solved
-            active_dict['score'] = active_game._color_score
-            active_dict['moves'] = cur_moves
-            active_dict['legal_moves'] = {}
-            active_dict['moves'].append(move)
-            active_dict['hash'] = hash(active_game)
+            active_game = deepcopy(game)
+            active_game._push_move(*move)
+            active_dict.update({
+                'game': active_game,
+                'solved': active_game._solved,
+                'score': active_game._color_score,
+                'moves': cur_moves + [move],
+                'legal_moves': {},
+                'hash': hash(active_game),
+            })
             # Leave for debugging.
             # print(active_dict['moves'])
             if hash(active_dict['game']) not in visited:
